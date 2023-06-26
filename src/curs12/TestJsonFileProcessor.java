14dd84d4f62762fee71e854f956c3a01946c6ae9
Dragos Tanta <dragos.tanta@gmail.com>
2023-06-26 10:26:59 +0300

    curs13 intermediar

--- src/curs12/TestJsonFileProcessor.java
+++ src/curs12/TestJsonFileProcessor.java
@@ -7,6 +7,17 @@ public class TestJsonFileProcessor {
 		
 		JsonFileProcesor obj =  new JsonFileProcesor();
 		obj.writeJsonFile();
+		obj.readJson("name");
+		obj.readJson("listaObiecte");
+		obj.readJson("email");
+		System.out.println("--------------------------------");
+		
+		obj.updateJsonFile("email", "altEmail");
+		System.out.println("Email after update :");
+		obj.readJson("email");
+		obj.deleteFromJson("email");
+		System.out.println("Email after delete :");
+		obj.readJson("email");
 		
 		
 	}
